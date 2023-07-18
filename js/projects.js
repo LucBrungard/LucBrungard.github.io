dc41f406ebc2eca9f4ba90936d96a84dc31525df
LucBrungard <luc.brungard@laposte.net>
2023-07-18 17:10:40 +0200

    qfe

--- js/projects.js
+++ js/projects.js
@@ -1,25 +1,40 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const host = 'lucbrungard.github.io'
-    const rootPath = window.location.href.substring(0, window.location.href.lastIndexOf(host) + host.length)
+    // const host = 'lucbrungard.github.io'
+    // const rootPath = window.location.href.substring(0, window.location.href.lastIndexOf(host) + host.length)
 
-    const projects = await (await fetch(`${rootPath}/projects.json`)).json()
+    // const projects = await (await fetch(`${rootPath}/projects.json`)).json()
 
-    const container = document.getElementById('container')
-    for (const semester in projects) {
-        const semesterTitle = document.createElement("h2")
-        semesterTitle.classList.add("semestre")
-        semesterTitle.innerHTML = semester
-        container.appendChild(semesterTitle)
+    // const container = document.getElementById('container')
+    // for (const semester in projects) {
+    //     const semesterContainer = document.createElement("div")
+    //     semesterContainer.classList.add("semestre")
+    //     container.appendChild(semesterContainer)
 
-        for (const projet of projects[semester]) {
-            const projectDiv = document.createElement("div")
-            projectDiv.classList.add("projet")
-            semesterTitle.appendChild(projectDiv)
+    //     const semesterTitle = document.createElement("h2")
+    //     semesterTitle.innerHTML = semester
+    //     semesterContainer.appendChild(semesterTitle)
 
-            console.log(projet)
-        }
-    }
+    //     for (const project of projects[semester]) {
+    //         const projectDiv = document.createElement("div")
+    //         projectDiv.classList.add("projet")
+    //         semesterContainer.appendChild(projectDiv)
+
+    //         const projectTitle = document.createElement("a")
+
+    //         const projectText = document.createElement("h2")
+    //         projectText.innerHTML = project.title
+    //         projectTitle.appendChild(projectText)
+
+    //         const projectCarret = document.createElement("i")
+    //         projectCarret.classList.add('fa-solid', 'fa-caret-up')
+    //         projectCarret.style.color = '#ff963c'
+    //         projectTitle.appendChild(projectCarret)
+
+    //         const projectContent = document.createElement("div")
+    //         projectDiv.appendChild(projectContent)
+    //     }
+    // }
 })
 
 function roule(varchar) {
